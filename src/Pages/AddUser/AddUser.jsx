6fcb2d0b87afaaa967f53061904f29d98f74ed1c
Miraz9907 <meraz.rahman99@gmail.com>
2024-02-12 14:49:23 +0600

    sort users

--- src/Pages/AddUser/AddUser.jsx
+++ src/Pages/AddUser/AddUser.jsx
@@ -1,16 +1,105 @@
 
 
 const AddUser = () => {
-    return (
-        <div className=""> 
-            <h2>Add New User </h2>
-            <form className="flex flex-col gap-3 justify-items-center align-middle ">
-            <input type="file" placeholder="Type here" className="input input-bordered input-primary w-full max-w-xs" />
-            <input type="text" placeholder="Type here" className="input input-bordered input-primary w-full max-w-xs" />
-            <input type="email" placeholder="Type here" className="input input-bordered input-primary w-full max-w-xs" />
-            <input type="text" placeholder="Type here" className="input input-bordered input-primary w-full max-w-xs" />
-            <input type="text" placeholder="Type here" className="input input-bordered input-primary w-full max-w-xs" />
 
+    const handleSignup = event =>{
+        event.preventDefault();
+        const form = event.target;
+        const avatar = form.avatar.value;
+        const firstName = form.firstname.value;
+        const lastName = form.lastname.value;
+        const email = form.email.value;
+        const address = form.address.value;
+        const companyName = form.company.value;
+
+        console.log(avatar,firstName,lastName,email,address,companyName)
+    }
+    return (
+        <div className="mt-16"> 
+            <h2 className="text-3xl text-center">Add New User </h2>
+            
+
+    <form onSubmit={handleSignup} className="card-body lg:w-1/2 m-auto">
+            <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Avatar</span>
+                </label>
+                <input
+                  type="file"
+                  name='avatar'
+                  accept=".jpg, .jpeg .png" 
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">First Name</span>
+                </label>
+                <input
+                  type="text"
+                  name='firstname'
+                  placeholder="First name"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Last Name</span>
+                </label>
+                <input
+                  type="text"
+                  name='lastname'
+                  placeholder="Last name"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="text"
+                  name='email'
+                  placeholder="email"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Address</span>
+                </label>
+                <input
+                  type="text"
+                  name='address'
+                  placeholder="address"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Company Name</span>
+                </label>
+                <input
+                  type="text"
+                  name='company'
+                  placeholder="Company Name"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+             
+              <div className="form-control mt-6">
+                <input
+                  className="btn btn-primary"
+                  type="submit"
+                  value="Add User"
+                />
+              </div>
             </form>
         </div>
     );
--- src/Pages/Users/Users.jsx
+++ src/Pages/Users/Users.jsx
@@ -4,6 +4,8 @@ import User from '../User/User';
 const Users = () => {
     const [users, setUsers] = useState([]);
     const [searchUser, setSearchUsers] = useState([]);
+    const [sortBy, setSortBy] = useState('');
+
 
     useEffect( ()=>{
         const url = 'https://dummyjson.com/users';
@@ -19,17 +21,38 @@ const Users = () => {
     const onSearch = (e) =>{
         setSearchUsers(users.filter(user => user.username.toLowerCase().includes(e.target.value)))
     }
+
+
+
+
+    const handleSortChange = (e) => {
+        const selectedSort = e.target.value;
+        setSortBy(selectedSort);
+        sortUsers(selectedSort);
+      };
+      const sortUsers = (sortKey) => {
+        const sortedUsers = [...users].sort((a, b) => {
+          if (a[sortKey] < b[sortKey]) return -1;
+          if (a[sortKey] > b[sortKey]) return 1;
+          return 0;
+        });
+    
+        setSearchUsers(sortedUsers);
+      };
+
     return (
         <div>
 
         <div className="sm:flex-col md:flex-row sm:m-auto p-10">
             <input onChange={onSearch} type="text" placeholder="Search here" className="input input-bordered input-primary w-full m-4 max-w-xs" />
 
-        <select className="select select-bordered w-full m-4 max-w-xs">
-            <option disabled value="Sort by name">Sort by name</option>
-            <option>Sort by email defau</option>
-            <option>Sort by Company name</option>
-        </select>
+
+            <label htmlFor="sortSelect">Sort By:</label>
+            <select id="sortSelect" value={sortBy} onChange={handleSortChange} className="select select-bordered w-full m-4 max-w-xs">
+                <option value="name">Name</option>
+                <option value="email">Email</option>
+                <option value="companyName">Company Name</option>
+            </select>
         </div>
             
             <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 px-10 '>
